d26d2d216b1a1fe2ddf9f24674481b204c09121a
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2025-08-18 15:23:41 +0300

    run pipeline solar system

--- terraform/2-variables.tf
+++ terraform/2-variables.tf
@@ -12,12 +12,12 @@ variable "node_groups" {
 
   default = {
     "default" = {
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
-        desired_size = 2
-        max_size     = 4
-        min_size     = 1
+        desired_size = 3
+        max_size     = 5
+        min_size     = 2
       }
     }
   }
