5c316f342a12530487e64ec227f860596fa1b9c9
Merna Islam <mernaislam39@gmail.com>
2025-08-23 16:09:30 +0300

    run workflow

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,13 @@ WORKDIR /usr/app
 COPY package*.json .
 
 RUN npm install
+RUN npm install prom-client
 
 COPY . .
 
-ENV MONGO_USERNAME=""
-ENV MONGO_PASSWORD=""
-ENV MONGO_URI=""
+ENV MONGO_USERNAME="superuser"
+ENV MONGO_PASSWORD="SuperPassword"
+ENV MONGO_URI="mongodb+srv://supercluster.d83jj.mongodb.net/superData"
 
 EXPOSE 3000
 
--- app.js
+++ app.js
@@ -3,9 +3,33 @@ const express = require('express');
 const OS = require('os');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const client = require('prom-client');
 const app = express();
 const cors = require('cors')
 
+// Initialize Prometheus metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// Custom metric: count HTTP requests
+const httpRequests = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "statusCode"],
+});
+register.registerMetric(httpRequests);
+
+// Middleware to increment counter
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequests.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+  });
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
@@ -78,6 +102,12 @@ app.get('/ready',   function(req, res) {
         "status": "ready"
     });
 })
+
+// Expose metrics at /metrics
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
 
 app.listen(3000, () => {
     console.log("Server successfully running on port - " +3000);
